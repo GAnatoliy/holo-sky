6ef9a10f6536c2d00f270df3c263002c470ae8c7
Oleg Semenov <o_semenov@mifprojects.com>
2020-10-03 18:18:18 +0300

    Added main manager

---
+++ UnityProj/Assets/Scripts/MainManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+
+namespace Assets.Scripts
+{
+    public class MainManager : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
