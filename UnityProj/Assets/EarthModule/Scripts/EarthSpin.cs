429cb2b0f217ec1566a6dc43755104619bde69f7
Vladimir Yaremenko <v_yaremenko@mifprojects.com>
2020-10-03 12:09:52 +0300

    Add earth spin component

--- UnityProj/Assets/EarthModule/Scripts/EarthSpin.cs
+++ UnityProj/Assets/EarthModule/Scripts/EarthSpin.cs
@@ -3,9 +3,13 @@
 public class EarthSpin : MonoBehaviour
 {
     public float Acceleration = 1.0f;
-
+    
     private void Update()
     {
-        
+        var daytimeInSeconds = 24 * 60 * 60;
+
+        var clockwiseAxis = Vector3.down;
+
+        transform.Rotate(clockwiseAxis, Acceleration * 360 / daytimeInSeconds * Time.deltaTime, Space.Self);
     }
 }
