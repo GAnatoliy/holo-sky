9f3b01aadb54f2a68e8dfc44d379021c173da63d
Vladimir Yaremenko <v_yaremenko@mifprojects.com>
2020-10-04 11:27:54 +0300

    Add ground stations manager

--- UnityProj/Assets/EarthModule/Scripts/GeoObject.cs
+++ UnityProj/Assets/EarthModule/Scripts/GeoObject.cs
@@ -7,19 +7,19 @@ public class GeoObject : MonoBehaviour
     public Transform Origin;
 
     // Update is called once per frame
-    void Update()
-    {
+    //void Update()
+    //{
         //var position = Origin.position + /*Quaternion.Euler(new Vector3(Latitude, Longitude)) **/ Origin.forward * 20f;
         //var localPos = Quaternion.Euler(new Vector3(-Latitude, -Longitude)) * new Vector3(0, 0, 20);
 
         //transform.localPosition = localPos;
 
         //transform.position = position.RotateAround(Origin.position, new Vector3(Latitude, Longitude));
-        SetCoordinates(Latitude, Longitude);
+        //SetCoordinates(Latitude, Longitude);
         //transform.position = position;
         //transform.RotateAround(Origin.position, new Vector3(1, 0, 0), Latitude);
         //transform.RotateAround(Origin.position, new Vector3(0, 1, 0), Longitude);
-    }
+    //}
 
     public void SetCoordinates(double latitude, double longitude)
     {
